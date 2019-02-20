7d75a35787a0eea37fd1f2114e6144369eb8f183
flndr <alexia.revoluzioner@gmail.com>
2019-02-20 03:27:27 +0200

    active and new charities output was added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(CharitiesTableSeeder::class);
+        $this->call(CharityStatusesTableSeeder::class);
+        $this->call(ClientTableSeeder::class);
+        $this->call(StaffTableSeeder::class);
     }
 }
